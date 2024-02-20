588f87e85e7b0e4ef2a0046652cdfe4c6ef467ff
Full Name <fullname@torradeira>
2024-02-20 12:18:40 +0000

    basic func

--- tests/example_tester.py
+++ tests/example_tester.py
@@ -1,14 +1,14 @@
+
 config = speech.RecognitionConfig(
-  language_code="pt",
-  enable_automatic_punctuation=True,
-  enable_word_time_offsets=True,
+    language_code="fr",
+	# language_code="en",
+    enable_automatic_punctuation=True,
+    enable_word_time_offsets=True,
 )
-
-audio = speech.RecognitionAudio()
-with open("../audio_manel.ogg", "rb") as audio_file:
-    audio_data = audio_file.read()
-
-audio.content = audio_data
+audio = speech.RecognitionAudio(
+  uri="gs://cloud-samples-data/speech/corbeau_renard.flac",
+#    uri="gs://cloud-samples-data/speech/brooklyn_bridge.flac",
+)
 
 response = speech_to_text(config, audio)
 print_response(response)
