f5eb4f717cee76e678204663fcb0a0e6ad8185bf
Duarte Flores <duartevieiramagalhaes@gmail.com>
2024-02-22 17:47:45 +0000

    fixed the fix

--- NOVONOVO/ui/main.js
+++ NOVONOVO/ui/main.js
@@ -15,7 +15,7 @@ function enviarAudioParaServidor(blob) {
     let worker_number = document.getElementById('workerNumberInput').value;
     formData.append('worker_number', worker_number);
 
-    fetch('http://localhost:5000/upload-recorded', {
+    fetch('http://localhost:5000/upload', {
         method: 'POST',
         body: formData
     })
