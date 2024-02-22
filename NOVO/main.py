2d79ec6dd895975eb7fd52fd156000a03bd92fa3
Full Name <fullname@torradeira>
2024-02-22 15:05:27 +0000

    ola

--- NOVO/main.py
+++ NOVO/main.py
@@ -1,8 +1,10 @@
 import os
 from flask import Flask, request
 from flask import jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/processManualUpload', methods=['POST'])
 def processManualUpload():
